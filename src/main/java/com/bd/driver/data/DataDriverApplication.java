861e1cf023610b43973bbb100cad526f94235406
dima.beselea <Genius.23>
2021-07-27 20:47:01 +0300

    Init commit

---
+++ src/main/java/com/bd/driver/data/DataDriverApplication.java
@@ -0,0 +1,13 @@
+package com.bd.driver.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataDriverApplication.class, args);
+	}
+
+}
